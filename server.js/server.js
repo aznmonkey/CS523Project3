056e73f07c4f3df75a0bed856f4755af9510babf
Arthur Nishimoto <arthur.nishimoto@gmail.com>
2017-04-29 01:20:09 -0500

    Fixed image sending to server (multi-packet check)

--- server.js/server.js
+++ server.js/server.js
@@ -1,165 +1,101 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var formidable = require('formidable');
 var fs = require('fs');
-var socketio = require('socket.io');
-var chokidar = require('chokidar');
-// Use python shell
-var PythonShell = require('python-shell');
+var spawn = require('child_process').spawn;
 
-var uploadPath = '/uploads';
-var processedPath = '/processed';
+var server = app.listen(11523, function(){
+  console.log('Server listening on port 11523');
+});
 
-//   /* arthur's paths */
-var pythonExecutable = '/home/evl/anishi2/bin/python3.5';
-var pythonScriptPath = "/data/evl/anishi2/cs523/CS523Project1/";
-
-/* kristine's paths */
-//var pythonExecutable = "/usr/bin/python"
-//var pythonScriptPath = "/Users/kristinelee/Desktop/class/523/p1/neural-style-master/";
-var pythonScript = "neural_style.py"
-var pythonNetworkPath = pythonScriptPath+"imagenet-vgg-verydeep-19.mat"
-
-// Set environment variables
-// LD_LIBRARY_PATH is not read by default? (not needed if using ssh)
-process.env.LD_LIBRARY_PATH = "/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64";
-
-// Function to convert an Uint8Array to a string
-var uint8arrayToString = function(data){
-    return String.fromCharCode.apply(null, data);
-};
-
-var pythonOptions = {
-	mode: 'text',
-    pythonPath: pythonExecutable,
-    pythonOptions: ['-u'],
-    scriptPath: pythonScriptPath,
-    args: []
-};
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, 'views/index.html'));
-});
-
 app.post('/upload', function(req, res){
-
-  // create an incoming form object
-  var form = new formidable.IncomingForm();
-
-  // specify that we want to allow the user to upload multiple files in a single request
-  form.multiples = true;
-
-  // store all uploads in the /uploads directory
-  form.uploadDir = path.join(__dirname, uploadPath);
+    var dataString;
 	
-  var processedImageDir = path.join(__dirname, processedPath);
+    req.on('data', function(data) {
+		var regex = /^data:.+\/(.+);base64,(.*)$/;
+		var matches = String(data).match(regex);
+		if (matches !== null) {
+			var ext = matches[1];
+			dataString = matches[2];
+			//fs.writeFileSync('data.' + ext, buffer);
+			console.log("Starting packet received");
+		} else {
+			console.log("partial image packet received");
+			dataString += data;
+		}
+    });
 
-  var contentPath = null;
-  var stylePath = null;
+    req.on('end', function() {
+		console.log("Full packet received?");
+		var buffer = new Buffer(dataString, 'base64');
+        if (buffer) {
+            var pathString = path.join(__dirname, 'public/images/output.jpg');
+            var success = false;
 
-  // every time a file has been uploaded successfully,
-  // rename it to it's orignal name
-  form.on('file', function(field, file) {
-    var newPath = form.uploadDir + '/' + field + '/' + file.name;
-    fs.rename(file.path, newPath);
-    if (field == 'content') {
-        contentPath = newPath;
-    }
-    else {
-        stylePath = newPath;
-    }
-	
-	const spawn = require('child_process').spawn;
+            fs.writeFile(
+                pathString,
+                buffer,
+                'base64',
+                function(err) {
+                    if (err) {
+                        console.log("Error writing image", err);
+                    }
+                    else {
+                        console.log("Sending to the shell...")
+                        var process = processFile(pathString);
 
-    if (!contentPath || !stylePath) { 
-        return; 
-    }
-	
-	// Lyra SSH version
-	pythonArgs = 
-	[
-		"lyra-02",
-		"nohup",
-		pythonExecutable,
-		pythonScriptPath+pythonScript,
-	 	"--content", contentPath,
-	 	"--styles", stylePath,
-	 	"--output", processedImageDir+"/"+file.name,
-	 	"--iterations", 10,
-		"--network", pythonNetworkPath
-	]
-	
-	console.log('running...');
-	console.log(pythonArgs);
-//   /* running on lyra */
-   const scriptExecution = spawn("ssh", pythonArgs);
-	//const scriptExecution = spawn(pythonExecutable, pythonArgs.slice(3));
-	
-    /* running on local */
-    //const scriptExecution = spawn("python", pythonArgs.slice(3));
-	
-	// Handle normal output
-	scriptExecution.stdout.on('data', (data) => {
-		s && s.emit("fileUploaded", {file: "blah", image: data});
+                        process.stdout.on('data', function(msg) {
+                            var string = parseString(msg);
+                            if (string.includes('wrote index at')) {
+                                success = true;
+                            }
+                            console.log('+', string);
+                        });
 
-		console.log(uint8arrayToString(data));
-		console.log(data);
-	});
+                        process.stderr.on('data', function(err) {
+                            console.log('-', parseString(err));
+                        });
 
-	// Handle error output
-	scriptExecution.stderr.on('data', (data) => {
-		// As said before, convert the Uint8Array to a readable string.
-		console.log(uint8arrayToString(data));
-	});
+                        process.on('exit', function(data) {
+                            console.log('done', data);
+                            if (data == 0 && success) {
+                                res.sendStatus(200);
+                                console.log('success');
+                            }
+                            else {
+                                res.sendStatus(500);
+                                console.log('error');
+                            }
+                        });
 
-	scriptExecution.on('exit', (code) => {
-		console.log("Process quit with code : " + code);
-		fs.readFile(path.join(__dirname, processedPath) + '/' +  file.name, function(err, data) {
-            if (err) {
-                console.log(err);
-				return;
-            }
-            else {
-                s.emit('fileUploaded', { file: processedPath + '/' +  file.name, image: data });
-            }
-        })
-
-	});
-
-  });
-
-  // log any errors that occur
-  form.on('error', function(err) {
-    console.log('An error has occured: \n' + err);
-  });
-
-  // once all the files have been uploaded, send a response to the client
-  form.on('end', function() {
-    res.end('success');
-  });
-
-  // parse the incoming request containing the form data
-  form.parse(req);
-
+                    }
+                }
+            );            
+        }
+    });
 });
 
-var server = app.listen(10523, function(){
-  console.log('Server listening on port 10523');
-});
+function parseString(data) {
+    return String.fromCharCode.apply(null, data);
+}
 
-const io = socketio(server);
-let s;
-io.on('connection', function(socket) {
-	s = socket;
-    fs.watch(path.join(__dirname, processedPath), (evt, filename) => {
+function processFile(pathString) {
+    var scriptPath = '/data/evl/anishi2/cs523/CS523Project3/pix2pix/';
+    var script = 'pix2pix_testPkm';
 
-      if (filename) {
-
-      } else {
-        console.log('filename not provided');
-      }
-    });    
-})
+    var args = [
+        "lyra-02",
+        "nohup",
+        scriptPath + script
+    ];
+   
+    console.log('running'); 
+    /* running on lyra */
+    return spawn('ssh', args);
+}
